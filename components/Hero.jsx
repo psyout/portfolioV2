cbd71d59824df0a09d75762512111271ca74d01c
Felipe Gonzalez <contraviento@gmail.com>
2024-07-03 16:01:01 +0100

    create contact and download resume button on hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,15 +18,17 @@ const Hero = () => {
 						<h4 className='text-sm uppercase font-semibold mb-4 text-primary tracking-[3px]'>Front-End Web Developer</h4>
 						<h1 className='h1 mb-4'>Hello, my name is Felipe </h1>
 						<p className='subtitle max-w-[490px] mx-auto xl:mx-0'>Building simple and beautiful things for complex interfaces is what I enjoy most.</p>
+
+						{/* buttons */}
 						<div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
 							<Link href='/contact'>
 								<Button className='gap-x-2'>
 									Contact me <Send size={18} />
 								</Button>
-								<Button className='gap-x-2'>
-									Contact me <Send size={18} />
-								</Button>
 							</Link>
+							<Button variant='secondary' className='gap-x-2'>
+								Download Resume <Download size={18} />
+							</Button>
 						</div>
 					</div>
 					<div className='hidden xl:flex relative'>Image</div>
